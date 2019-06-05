0df1f7f62f1b0597c9f7f30aecb52053414f6891
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2019-06-05 12:39:43 +0200

    test

--- src/view/PlayerPane.java
+++ src/view/PlayerPane.java
@@ -2,32 +2,43 @@ package view;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import model.Board;
 
 public class PlayerPane extends Pane{
 	public final int width = 770;
 	public int unit;
-
+	public Board board;
+	int x = unit/2;
+	int y = width/2;
+	
 	public PlayerPane() {
 		super();
+		this.board = new Board();
 		int unit = width/11;
-		Rectangle rHY1 = new Rectangle(unit/2, unit/2);
+		/*Rectangle rHY1 = new Rectangle(unit/2, unit/2);
 		Rectangle rHY2 = new Rectangle(100, 30,unit/2, unit/2);
 		Rectangle rHY3= new Rectangle(50, 400,unit/2, unit/2);
 		Rectangle rHY4= new Rectangle(700, 30,unit/2, unit/2);
 		Rectangle rHY5 = new Rectangle(50, 250, unit/2, unit/2);
+		*/
 		
+		Circle circle1 = new Circle(x, y, unit/3, Color.WHITE);
+		circle1.setStroke(Color.BLACK);
 		
 		//Polygon triangle = new Polygon();
 		//triangle.getPoints().addAll(50.0, 0.0, 0.0, 50.0, 100.0, 50.0);
 		//triangle.setFill(Color.BLACK);
 		//triangle.setStroke(Color.RED); 
 
-		getChildren().addAll(rHY1,rHY2,rHY3,rHY4,rHY5);
+		
+		
+		
+		
+		getChildren().addAll(circle1);
 	}
 
-	public void setRectangles() {
-		
-	}
+	
 }
--- src/view/ScenePane.java
+++ src/view/ScenePane.java
@@ -1,21 +1,60 @@
 package view;
 
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
 
 
 public class ScenePane extends BorderPane{
 
 	private PlayerPane playerPane;
 	private InfoPane infoPane;
+	/*private DicePane dicePane;
+	private int id;
+	private Color playerColor;
+	private ProgressBar progressBar;
+	private MenuBar menuBar;
+	
+	private Label label;
+	private String labelDisable;
+	private String labelEnable;
+	private String labelWin;
+	private String labelLose;
+	*/
 	
 //in den Konstruktor die DicePane und InfoPane geben
 	public ScenePane(PlayerPane playerPane, InfoPane infoPane) {
 		super();
 		this.playerPane = playerPane;
 		this.infoPane = infoPane;
+		/*this.dicePane = dicePane;
+		this.id = id;
+		
+		if(id == 1) {
+			playerColor = Color.YELLOW;
+		} else if(id == 2) {
+			playerColor = Color.GREEN;
+		} else if(id == 3) {
+			playerColor = Color.BLUE;
+		} else if(id == 4) {
+			playerColor = Color.DARKRED;
+		}
+		
+		 this.menuBar = menuBar;
+		*progressBar = new ProgressBar();
+		*progressBar.setPrefWidth(120);
+		*label = new Label("");
+		*int id1 = id%2 +1;
+		*labelDisable = "Warten auf Spieler " + id1;
+		*labelEnable = "Machen Sie Ihren Zug";
+		*labelWin = "Sie haben gewonnen!";
+		*labelLose = "Sie haben verloren!";
+		*/
 		init();
+		//this.getChildren().add(menuBar);
 	}
 
 
@@ -27,5 +66,24 @@ public class ScenePane extends BorderPane{
 		AnchorPane.setBottomAnchor(infoPane, 990.);
 		this.setCenter(center);
 	}
+	
+	/*public void disable(){
+		label.setText(labelDisable);
+		progressBar.setProgress(-1);
+	}
+	
+	public void enable(){
+		label.setText(labelEnable);
+		progressBar.setProgress(1);
+	}
+	
+	public void win(){
+		label.setText(labelWin);
+	}
+	
+	public void lose(){
+		label.setText(labelLose);
+	}
+*/
 
 }
